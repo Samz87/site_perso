fddde972e1b46d3994aeb9b001d6de53ff9f9f01
Sam Montaudon <sam.montaudon@gmail.com>
2020-07-14 18:27:55 +0200

    sections homepage

--- views/homepage/index.php
+++ views/homepage/index.php
@@ -1,5 +1,30 @@
 <?php require_once('views/templates/header.php'); ?>
-<main>
-    <p>Homepage</p>
-</main>
+
+<body>
+
+    <main class="homepage_main">
+        <section>
+            <img src="http://placehold.it/500x200" alt="">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure fugit sapiente maxime, voluptate voluptatum aliquid provident iste aperiam assumenda accusantium asperiores eligendi esse ducimus dolore ad quis illum quibusdam modi, tenetur obcaecati facilis temporibus. Distinctio doloribus modi consectetur cum est ullam eaque rem ad officiis culpa? Iste fugiat recusandae quae!</p>
+        </section>
+        <section>
+            <div class="card">
+                <p>A propos</p>
+            </div>
+            <div class="card">
+                <p>Mes compétences</p>
+            </div>
+            <div class="card">
+                <p>Objectifs</p>
+            </div>
+            <div class="card">
+                <p>Actualités</p>
+            </div>
+            <div class="card">
+                <p>Me contacter</p>
+            </div>
+        </section>
+
+    </main>
+</body>
 <?php require_once('views/templates/footer.php');
