a5a3cf61dd2141acd1555305fe15024eeee9e6c5
Samz87 <sam.montaudon@gmail.com>
2020-07-17 15:52:41 +0200

    reglages

--- views/homepage/index.php
+++ views/homepage/index.php
@@ -2,8 +2,8 @@
 
 <body>
 
-    <!-- <main class="homepage_main">
-        <section>
+<main class="homepage_main">
+        <!--<section>
             <img src="http://placehold.it/500x200" alt="">
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure fugit sapiente maxime, voluptate voluptatum aliquid provident iste aperiam assumenda accusantium asperiores eligendi esse ducimus dolore ad quis illum quibusdam modi, tenetur obcaecati facilis temporibus. Distinctio doloribus modi consectetur cum est ullam eaque rem ad officiis culpa? Iste fugiat recusandae quae!</p>
         </section>
@@ -23,8 +23,8 @@
             <div class="card">
                 <p>Me contacter</p>
             </div>
-        </section>
+        </section> -->
 
     </main>
-</body> -->
+</body>
 <?php require_once('views/templates/footer.php');
