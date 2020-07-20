70a96a20b7ea8d1b45e1f7c2a03f067b74ca7922
Samz87 <sam.montaudon@gmail.com>
2020-07-20 16:21:25 +0200

    page objectifs

--- views/objectives/index.php
+++ views/objectives/index.php
@@ -1,5 +1,51 @@
 <?php require_once('views/templates/header.php'); ?>
 <main>
-    <p>Objectifs</p>
+    <div class="container-fluid objectives">
+        <div class="row">
+            <div class="col-md-6 col-xs-12 pro-obj">
+                <h2>Objectifs professionnels</h2>
+                <div class="obj">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, nemo voluptatibus esse neque, officia veritatis et est suscipit aut voluptate dolore nesciunt dolores recusandae libero quis maiores quam beatae eos. Qui quisquam dolor, quis aspernatur optio fugiat cupiditate quos commodi!</p>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-striped progress-bar-animated" style="width:70%"></div>
+                    </div>
+                </div>
+                <div class="obj">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, nemo voluptatibus esse neque, officia veritatis et est suscipit aut voluptate dolore nesciunt dolores recusandae libero quis maiores quam beatae eos. Qui quisquam dolor, quis aspernatur optio fugiat cupiditate quos commodi!</p>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-striped progress-bar-animated" style="width:70%"></div>
+                    </div>
+                </div>
+                <div class="obj">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, nemo voluptatibus esse neque, officia veritatis et est suscipit aut voluptate dolore nesciunt dolores recusandae libero quis maiores quam beatae eos. Qui quisquam dolor, quis aspernatur optio fugiat cupiditate quos commodi!</p>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-striped progress-bar-animated" style="width:70%"></div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-md-6 col-xs-12 perso-obj">
+                <h2>Objectifs personnels</h2>
+                <div class="obj">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, nemo voluptatibus esse neque, officia veritatis et est suscipit aut voluptate dolore nesciunt dolores recusandae libero quis maiores quam beatae eos. Qui quisquam dolor, quis aspernatur optio fugiat cupiditate quos commodi!</p>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-striped progress-bar-animated" style="width:70%"></div>
+                    </div>
+                </div>
+                <div class="obj">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, nemo voluptatibus esse neque, officia veritatis et est suscipit aut voluptate dolore nesciunt dolores recusandae libero quis maiores quam beatae eos. Qui quisquam dolor, quis aspernatur optio fugiat cupiditate quos commodi!</p>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-striped progress-bar-animated" style="width:70%"></div>
+                    </div>
+                </div>
+                <div class="obj">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, nemo voluptatibus esse neque, officia veritatis et est suscipit aut voluptate dolore nesciunt dolores recusandae libero quis maiores quam beatae eos. Qui quisquam dolor, quis aspernatur optio fugiat cupiditate quos commodi!</p>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-striped progress-bar-animated" style="width:70%"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </main>
 <?php require_once('views/templates/footer.php');
