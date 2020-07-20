bc032797a02bcd81878c4bf9e21e1ebc3b1342ca
Samz87 <sam.montaudon@gmail.com>
2020-07-20 17:24:40 +0200

    logos contact

--- views/contact/index.php
+++ views/contact/index.php
@@ -1,5 +1,17 @@
 <?php require_once('views/templates/header.php'); ?>
 <main>
-    <p>Contact</p>
+    <div class="container contact-container">
+        <div class="row">
+            <div class="col-6 contact-links">
+                <i class="fa fa-phone fa-3x" aria-hidden="true"></i>
+                <i class="fa fa-github fa-3x" aria-hidden="true"></i>
+                <i class="fa fa-envelope fa-3x" aria-hidden="true"></i>
+                <i class="fa fa-codepen fa-3x" aria-hidden="true"></i>
+                <i class="fa fa-paperclip fa-3x" aria-hidden="true"></i>
+                <i class="fa fa-linkedin fa-3x" aria-hidden="true"></i>
+            </div>
+            <div class="col-6 contact-map"></div>
+        </div>
+    </div>
 </main>
 <?php require_once('views/templates/footer.php');
