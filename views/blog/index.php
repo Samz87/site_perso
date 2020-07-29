e635aa9bdfef5701f9eb88cc76e2afade9576230
Sam <sam.montaudon@gmail.com>
2020-07-29 15:35:52 +0200

    style sliders

--- views/blog/index.php
+++ views/blog/index.php
@@ -9,19 +9,18 @@ if (isset($_SESSION['isLogged'])) {
         <div class="col-12">
             <h2>Mes projets</h2>
             <div class="slider">
-                    <?php while ($proj = $projs->fetch(PDO::FETCH_BOTH)) { ?>
-                        <?php if ($proj['categorie'] == 'Projets') { ?>
-                            <div class="card">
-                                <img class="card-img-top" src="<?php echo BASE_URL . $proj['img'] ?>" alt="<?php echo $proj['img_alt'] ?>">
-                                <div class="card-body">
-                                    <h5 class="card-title"><?php echo $proj['title'] ?></h5>
-                                    <h6 class="card-date text-muted"><?php echo $proj['dateCreation'] ?></h6>
-                                    <p class="card-text"><?php echo $proj['content'] ?></p>
-                                    <a href="https://<?php echo $proj['link'] ?>" class="btn btn-primary">Visiter le site</a>
-                                </div>
+                <?php while ($proj = $projs->fetch(PDO::FETCH_BOTH)) { ?>
+                    <?php if ($proj['categorie'] == 'Projets') { ?>
+                        <div class="card">
+                            <img class="card-img-top" id="project-img" src="<?php echo BASE_URL . $proj['img'] ?>" alt="<?php echo $proj['img_alt'] ?>">
+                            <div class="card-img-overlay">
+                                <h5 class="card-title" id="project-title"><?php echo $proj['title'] ?></h5>
+                                <h6 class="card-date text-muted"><?php echo $proj['dateCreation'] ?></h6>
+                                <a href="https://<?php echo $proj['link'] ?>" class="btn btn-primary">Visiter le site</a>
                             </div>
-                    <?php }
-                    }; ?>
+                        </div>
+                <?php }
+                }; ?>
             </div>
         </div>
         <div class="col-12">
@@ -29,7 +28,7 @@ if (isset($_SESSION['isLogged'])) {
             <div class="slider">
                 <?php while ($article = $articles->fetch(PDO::FETCH_BOTH)) { ?>
                     <?php if ($article['categorie'] == 'Articles') { ?>
-                        <div class="card">
+                        <div class="card article_card">
                             <img class="card-img-top" src="<?php echo BASE_URL . $article['img'] ?>" alt="<?php echo $article['img_alt'] ?>">
                             <div class="card-body">
                                 <h5 class="card-title"><?php echo $article['title'] ?></h5>
