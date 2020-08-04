e165e834d231883ca0c6e68347e6e91e8b9c943a
Sam Montaudon <sam.montaudon@gmail.com>
2020-08-04 13:04:59 +0200

    connect admin

--- views/admin/login.php
+++ views/admin/login.php
@@ -1,7 +1,27 @@
+<?php
+$pagetitle="Connexion admin";
+if (isset($_SESSION['isLogged'])) {
+    require_once('views/templates/header_admin.php');
+} else {
+    require_once('views/templates/header.php');
+} ?>
 <main>
-<form action="" method="post">
-    <input type="text" name="mail" placeholder="Enter your email" required>
-    <input type="password" name="pw" placeholder="Enter your password" required>
-    <input type="submit" value="Submit" name="adminconnect">
-</form>
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+                <form method="post">
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Adresse email</label>
+                        <input type="text" name="mail" class="form-control" placeholder="Email">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Mot de passe</label>
+                        <input type="password" class="form-control" name="pw" placeholder="Mot de passe">
+                    </div>
+                    <button type="submit" class="btn btn-primary" value="Connexion" name="adminconnect">Connexion</button>
+                </form>
+            </div>
+        </div>
+    </div>
 </main>
+<?php require_once('views/templates/footer.php');
